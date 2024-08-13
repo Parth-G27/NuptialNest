6cd315f3fdcdba1b89994a84ed58cd3a92668758
Parth <parthbidari13@gmail.com>
2024-08-14 03:50:17 +0530

    Database connection done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import Connection from './database/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 const PORT = 8000;
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PWD;
+
+Connection(username, password);
 
 app.listen(PORT, 
     () => {
