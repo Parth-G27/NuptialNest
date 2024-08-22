d805f45d4f9cd63bfeea9c62b78524b97dc9d674
Parth <parthbidari13@gmail.com>
2024-08-22 23:55:23 +0530

    Edit thing fixed

--- client/app/page.js
+++ client/app/page.js
@@ -1,26 +1,11 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-  // Example state and function if you want to update something on click
-  const [counter, setCounter] = useState(0);
-
-  const updateCounter = () => {
-    setCounter(counter + 1);
-  };
-
+  
   return (
     <main className={styles.main}>
       <div>
-        {nums.map((num) => (
-          <Link href={`/dyna/${num}`} passHref key={num}>
-            <button onClick={updateCounter}>Click {num}</button>
-          </Link>
-        ))}
+        Landing page
       </div>
     </main>
   );
